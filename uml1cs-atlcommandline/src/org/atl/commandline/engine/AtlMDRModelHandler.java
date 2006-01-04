dce5773d08f9e1f09d951f39b87b2315d2d8f8ec
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2006-01-04 13:39:43 +0000

    Re-add to svn

---
+++ uml1cs-atlcommandline/src/org/atl/commandline/engine/AtlMDRModelHandler.java
@@ -0,0 +1,66 @@
+/*
+ * Created on 1 juin 2004
+ *
+ */
+package org.atl.commandline.engine;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.atl.engine.repositories.mdr4atl.ASMMDRModel;
+import org.atl.engine.vm.nativelib.ASMModel;
+
+/**
+ * @author JOUAULT
+ * @author Dennis Wagelaar
+ */
+public class AtlMDRModelHandler extends AtlModelHandler{
+	
+	private ASMMDRModel atlmm;
+	private ASMMDRModel mofmm;	
+	public void saveModel(final ASMModel model) throws IOException {
+		saveModel(model, model.getName() + ".xmi");
+	}
+	
+	public void saveModel(final ASMModel model, String fileName)
+            throws IOException {
+		File file = new File(fileName);
+		final FileOutputStream out = new FileOutputStream(file);
+		((ASMMDRModel) model).save(out);
+	}
+
+	public ASMModel getAtl() {
+		return atlmm;
+	}
+
+	public ASMModel getMof() {
+		return mofmm;
+	}
+	
+	public ASMModel loadModel(String name, ASMModel metamodel, InputStream in)
+            throws Exception {
+		ASMModel ret = null;
+		ret = ASMMDRModel.loadASMMDRModel(name, (ASMMDRModel)metamodel, in, null);
+		return ret;
+	}
+	public ASMModel newModel(String name, ASMModel metamodel) throws Exception {
+		ASMModel ret = null;
+		ret = ASMMDRModel.newASMMDRModel(name, (ASMMDRModel)metamodel, null);
+		return ret;
+	}
+
+	protected AtlMDRModelHandler() throws Exception {
+		URL atlurl = AtlMDRModelHandler.class.getResource("resources/ATL-0.2.xmi");
+		mofmm = ASMMDRModel.createMOF(null);
+		atlmm = ASMMDRModel.loadASMMDRModel("ATL", mofmm, atlurl, null);
+	}
+	
+	public ASMModel getBuiltInMetaModel(String name) {
+		ASMModel ret = null;
+		
+		return ret;
+	}
+}
